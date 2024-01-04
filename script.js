035c26a1d66a08bd19d0876f8f47fb1a19481031
d0rq4 <dorotamalicka9@gmail.com>
2024-01-04 20:43:05 +0100

    added first chart set up

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let data;
+let rawData;
 const apiUrl = 'https://my.api.mockaroo.com/edi_project.json?key=655ef660';
 
 $.ajax({
@@ -8,59 +9,11 @@ $.ajax({
   success: function (response) {
     renderContent(response);
     data = response;
+    rawData = response;
   },
   error: function (status, error) {
     console.log('Error:', status, error);
   },
-});
-
-const xyValues = [
-  { x: 50, y: 7 },
-  { x: 60, y: 8 },
-  { x: 70, y: 8 },
-  { x: 80, y: 9 },
-  { x: 90, y: 9 },
-  { x: 100, y: 9 },
-  { x: 110, y: 10 },
-  { x: 120, y: 11 },
-  { x: 130, y: 14 },
-  { x: 140, y: 14 },
-  { x: 150, y: 15 },
-];
-
-const data2 = {
-  labels: ['Red', 'Blue', 'Yellow'],
-  datasets: [
-    {
-      label: 'My First Dataset',
-      data: [300, 50, 100],
-      backgroundColor: [
-        'rgb(255, 99, 132)',
-        'rgb(54, 162, 235)',
-        'rgb(255, 205, 86)',
-      ],
-      hoverOffset: 4,
-    },
-  ],
-};
-
-new Chart('chart_1', {
-  type: 'doughnut',
-  data: data2,
-});
-
-new Chart('chart_2', {
-  type: 'scatter',
-  data: {
-    datasets: [
-      {
-        pointRadius: 4,
-        pointBackgroundColor: 'rgb(255,255,255)',
-        data: xyValues,
-      },
-    ],
-  },
-  options: {},
 });
 
 let slide = 0;
@@ -153,8 +106,114 @@ function sortRecords() {
   }
 }
 
+// Nie dziala mi wyswietlenie za pierwszym razem, chlopaki spojrzcie pls
+function sortPhonesMpx(data) {
+  let amount12Mpix= 0;
+  let amount24Mpix= 0;
+  let amount48Mpix= 0;
+  let amount108Mpix= 0;
+  for (let x = 0; x < 100; x++) {
+    if (data[x].Brand == brands[slide]) {
+      if(data[x].Camera == "12Mpix"){
+        amount12Mpix += 1;
+      }
+      if(data[x].Camera == "24Mpix"){
+        amount24Mpix += 1;
+      }
+      if(data[x].Camera == "48Mpix"){
+        amount48Mpix += 1;
+      }
+      if(data[x].Camera == "108Mpix"){
+        amount108Mpix += 1;
+      }
+      counter++;
+    }
+  }
+  return [amount12Mpix, amount24Mpix, amount48Mpix, amount108Mpix]
+}
+
+const myChart1Data = {
+  labels: ['12Mpx', '24Mpx', '48Mpx', '108Mpx'],
+  datasets: [
+    {
+      label: "Mpx",
+      data: [0,0,0,0],
+      backgroundColor: [
+        'rgb(255, 99, 132)',
+        'rgb(54, 162, 235)',
+        'rgb(255, 205, 86)',
+        'rgb(160, 121, 176)',
+      ],
+      hoverOffset: 4,
+    },
+  ],
+};
+
+const myChart2data = {
+  labels: [2010,2011,2012,2013,2014,2015,2016,2017,2018,2019,2020,2021,2022,2023],
+  datasets: [{
+    label: 'Ilość wodoodpornych smartfonów',
+    data: [65, 59, 80, 81, 56, 55,40,40,11,10,11,23,24],
+    backgroundColor: [
+      'rgba(255, 0, 0, 0.2)',
+      'rgba(255, 127, 0, 0.2)',
+      'rgba(255, 255, 0, 0.2)',
+      'rgba(127, 255, 0, 0.2)',
+      'rgba(0, 255, 0, 0.2)',
+      'rgba(0, 255, 127, 0.2)',
+      'rgba(0, 255, 255, 0.2)',
+      'rgba(0, 127, 255, 0.2)',
+      'rgba(0, 0, 255, 0.2)',
+      'rgba(127, 0, 255, 0.2)',
+      'rgba(255, 0, 255, 0.2)',
+      'rgba(255, 0, 127, 0.2)',
+      'rgba(255, 0, 0, 0.2)'
+    ],
+    borderColor: [
+      'rgba(255, 128, 128, 0.2)',
+      'rgba(255, 191, 128, 0.2)',
+      'rgba(255, 255, 128, 0.2)',
+      'rgba(191, 255, 128, 0.2)',
+      'rgba(128, 255, 128, 0.2)',
+      'rgba(128, 255, 191, 0.2)',
+      'rgba(128, 255, 255, 0.2)',
+      'rgba(128, 191, 255, 0.2)',
+      'rgba(128, 128, 255, 0.2)',
+      'rgba(191, 128, 255, 0.2)',
+      'rgba(255, 128, 255, 0.2)',
+      'rgba(255, 128, 191, 0.2)',
+      'rgba(255, 128, 128, 0.2)'
+    ],
+    borderWidth: 1,
+    
+  }]};
+
+let myChart1 = new Chart('chart_1', {
+  type: 'doughnut',
+  data: myChart1Data,
+});
+
+ let myChart2 = new Chart('chart_2', {
+  type: 'bar',
+  data: myChart2data,
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true
+      }
+    }
+  },
+});
+
+function updateCharts(){
+  myChart1.data.datasets[0].data = sortPhonesMpx(rawData);
+  myChart1.update()
+  //do zrobienia update drugiego
+}
+
 $('#carouselExample').on('slid.bs.carousel', function () {
   slide = $('div.active').index();
   $('#data').empty();
   sortRecords();
+  updateCharts();
 });
